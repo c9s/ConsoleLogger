d5d4ef2d9f485383818e3bae9501f29d65292562
c9s <cornelius.howl@gmail.com>
2011-12-27 17:59:35 +0800

    First commit

---
+++ src/ConsoleLogger/Formatter.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * This file is part of the CLIFramework package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace ConsoleLogger;
+
+class Formatter
+{
+
+    protected $styles = array(
+        'red'          => array('fg' => 'red'),
+        'green'        => array('fg' => 'green'),
+        'white'        => array('fg' => 'white'),
+        'yellow'       => array('fg' => 'yellow'),
+        'strong_red'   => array('fg' => 'red', 'bold'  => 1),
+        'strong_green' => array('fg' => 'green','bold' => 1),
+        'strong_white' => array('fg' => 'white','bold' => 1),
+    );
+
+    protected $options = array(
+        'bold' => 1, 
+        'underscore' => 4, 
+        'blink' => 5, 
+        'reverse' => 7, 
+        'conceal' => 8
+    );
+
+    protected $foreground = array(
+        'black' => 30, 
+        'red' => 31, 
+        'green' => 32, 
+        'yellow' => 33, 
+        'blue' => 34, 
+        'magenta' => 35, 
+        'cyan' => 36, 
+        'white' => 37
+    );
+
+    protected $background = array(
+        'black' => 40, 
+        'red' => 41, 
+        'green' => 42, 
+        'yellow' => 43, 
+        'blue' => 44, 
+        'magenta' => 45, 
+        'cyan' => 46, 
+        'white' => 47
+    );
+
+    protected $supportsColors;
+
+    public function __construct()
+    {
+        $this->supportsColors = DIRECTORY_SEPARATOR != '\\' 
+                && function_exists('posix_isatty') && @posix_isatty(STDOUT);
+    }
+
+
+    public function addStyle( $name, $style )
+    {
+        $this->styles[ $name ] = $style;
+    }
+
+
+    /**
+     * Formats a text according to the given style or parameters.
+     *
+     * @param  string   $text  The text to style
+     * @param  string   $style A style name
+     *
+     * @return string The styled text
+     */
+    public function format($text = '', $style = 'none')
+    {
+        if (!$this->supportsColors)
+            return $text;
+
+        if ( $style == 'none' || ! isset($this->styles[$style]) )
+            return $text;
+
+        $parameters = $this->styles[$style];
+        $codes = array();
+
+        if (isset($parameters['fg']))
+            $codes[] = $this->foreground[$parameters['fg']];
+
+        if (isset($parameters['bg']))
+            $codes[] = $this->background[$parameters['bg']];
+
+        foreach ( $this->options as $option => $value ) {
+            if (isset($parameters[$option]) && $parameters[$option]) {
+                $codes[] = $value;
+            }
+        }
+
+        return "\033[".implode(';', $codes).'m'.$text."\033[0m";
+    }
+
+}
+
